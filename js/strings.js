7b53fa7ae752e3580503da27c22f4a985cb7ed65
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2024-08-13 09:29:56 +0300

    Create strings.js

---
+++ js/strings.js
@@ -0,0 +1,10 @@
+const texts = {
+    pageTitle: "Випадкові завдання",
+    resetButtonText: "Перезапустити гру",
+    mainHeading: "Випадкові завдання",
+    getTaskButtonText: "Отримати завдання",
+    initialTaskText: "Натисніть кнопку, щоб отримати завдання",
+    allTasksCompletedText: "Усі завдання виконані!",
+    gameRestartedText: "Гра перезапущена. Натисніть кнопку, щоб отримати завдання",
+    taskPrefix: "Ваше завдання:" 
+};
