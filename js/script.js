fbed3aa43b31400f11f55a9a257e5c646476fbe6
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2024-08-13 09:32:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 let tasks = [...initialTasks]; 
 
-document.getElementById('page-title').innerText = texts.pageTitle;
-document.getElementById('reset-button').innerText = texts.resetButtonText;
-document.getElementById('main-heading').innerText = texts.mainHeading;
-document.getElementById('get-task-button').innerText = texts.getTaskButtonText;
-document.getElementById('task').innerText = texts.initialTaskText;
+document.getElementById('page-title').innerText = strings.pageTitle;
+document.getElementById('reset-button').innerText = strings.resetButtonText;
+document.getElementById('main-heading').innerText = strings.mainHeading;
+document.getElementById('get-task-button').innerText = strings.getTaskButtonText;
+document.getElementById('task').innerText = strings.initialTaskText;
 
 function getTask() {
     if (tasks.length > 0) {
         const randomIndex = Math.floor(Math.random() * tasks.length);
         const task = tasks[randomIndex];
-        document.getElementById('task').innerText = `${texts.taskPrefix} ${task}`;
+        document.getElementById('task').innerText = `${strings.taskPrefix} ${task}`;
         tasks.splice(randomIndex, 1); 
     } else {
-        document.getElementById('task').innerText = texts.allTasksCompletedText;
+        document.getElementById('task').innerText = strings.allTasksCompletedText;
     }
 }
 
 function resetGame() {
     tasks = [...initialTasks]; 
-    document.getElementById('task').innerText = texts.gameRestartedText;
+    document.getElementById('task').innerText = strings.gameRestartedText;
 }
-
